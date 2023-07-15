def202eecd97d75fafc3e14cc9e9c7e1e1905b3e
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-07-15 18:22:23 -0400

    listening lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+
+function addingEventListener(addEventListener){
+    if(input){
+        input.addEventListener('click', addEventListener, true)
+    }
 }
